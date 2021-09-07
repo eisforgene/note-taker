e9b18028cbe90065ab57c45f09504fc7007800bf
Bomb Atomically <ieugenelee@gmail.com>
2021-09-07 15:36:49 -0700

    added routes for api and html

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// how to find the files in server.js
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); // links html and css files
+
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
+
+// turn on routes
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+// turn on connection to db and server
+app.listen(PORT, () => console.log('Now listening'));
+
