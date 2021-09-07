6f18b5d416821a15c4fdb0a03cbe86086d1a2adc
Bomb Atomically <ieugenelee@gmail.com>
2021-09-07 15:57:25 -0700

    api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,38 @@
+const app = require("express").Router(); // requires only the router function from express
+let db = require('../db/db.json');
+
+app.get("/api/notes", function (req, res) {
+    db = JSON.parse(fs.readFileSync("./db/db.json"))
+    console.log("GET", db)
+    res.json(db)
+})
+
+app.post("/api/notes", function (req, res) {
+    const save = {
+        id: Math.floor(Math.random() * 1202),
+        title: req.body.title,
+        text: req.body.text
+
+    }
+    db.push(save)
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), function (err, result) {
+        if (err) throw err;
+    })
+    console.log("POST", db)
+    res.json(db)
+})
+
+app.delete("/api/notes/:id", function (req, res) {
+    let data = []
+    for(let i=0;i<db.length;i++){
+        if(db[i].id != req.params.id){
+            data.push(db[i])
+        }
+    }
+    db=data
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), function (err, result) {
+        if (err) throw err;
+    })
+    console.log("DELETE", db)
+    res.json(db)
+})
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,14 @@
+const app = require("express").Router(); // requires only the router function from express
 const path = require('path');
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+})
+
+module.exports = app;
